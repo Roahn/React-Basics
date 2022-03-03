5234c074ce2f31958a77f9cb079015d806941bb7
Roahn <rohan.magar19@vit.edu>
2022-03-03 22:45:02 +0530

    First phase to create catalog by using object

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDom from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import './index.css'
+const Book1 =
+{
+    img:'https://images-eu.ssl-images-amazon.com/images/I/41+grDTP2FL._AC_UL604_SR604,400_.jpg',
+    Author:'Ankur Warikoo',
+    Title:'Do Epic Shit'
+}
+const Book2 =
+{
+    img:'https://images-eu.ssl-images-amazon.com/images/I/81N7FmJhbhL._AC_UL604_SR604,400_.jpg',
+    Author:'Gaur Gopal Das',
+    Title:'Lifes Amazing Secrets: How to Find Balance and Purpose in Your Life '
+}
+function Greeting() {
+    return (
+        <section className = "booklist">
+            <Book 
+            img = {Book1.img}
+            Title={Book1.Title}
+            Author={Book1.Author}
+            />
+            <Book 
+            img = {Book2.img}
+            Title={Book2.Title}
+            Author={Book2.Author}
+            />
+            
+            
+        </section>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    )
+}
+
+const Book = function (props) {
+    const {img,Title,Author} = props
+    return (
+    <article className='book' >
+        <img src={img}></img>
+        <h1>{Title}</h1>
+        <h4 > {Author}</h4>
+    </article>
+    )
+}
+
+ReactDom.render(<Greeting />, document.getElementById('root'));
